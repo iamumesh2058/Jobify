f8bf83fd58c3032c43cff1025b2dd5466a73ad3b
Umesh Gurung <umesh.grg2058@gmai.com>
2024-03-18 10:13:56 +0545

    Fixing user update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/user.model.js";
 import Job from "../models/job.model.js";
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 
 export const getCurrentUser = async (req, res) => {
@@ -23,8 +23,10 @@ export const updateUser = async (req, res) => {
     delete newUser.password;
 
     if(req.file){
-        const response = await cloudinary.v2.uploader.upload(req.file.path);
-        await fs.unlink(req.file.path);
+        const file = formatImage(req.file);
+
+        const response = await cloudinary.v2.uploader.upload(file);
+        
         newUser.avatar = response.secure_url;
         newUser.avatarPublicId = response.public_id;
     }
