8d3bc2b9442d4d630de91b403587d259cf44854c
iamumesh2058 <umesh.grg2058@gmail.com>
2025-03-07 11:05:30 +0545

    Making some adjustments

--- errors/customError.js
+++ errors/customError.js
@@ -1,13 +1,5 @@
 import { StatusCodes } from "http-status-codes";
 
-// export class NotFoundError extends Error {
-//     constructor(message){
-//         super(message);
-//         this.name = 'NotFoundError',
-//         this.statusCode = StatusCodes.NOT_FOUND;
-//     }
-// }
-
 export const NotFoundError = class extends Error {
     constructor(message){
         super(message);
